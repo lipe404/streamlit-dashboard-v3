3b89561b29dff89181a4f35e5a1ea7e658fb9da0
lipe404 <lipelopes8@gmail.com>
2025-08-14 14:36:16 -0300

    Correções

--- utils/visualizations.py
+++ utils/visualizations.py
@@ -1,6 +1,5 @@
 import geopandas as gpd
 import json
-# from utils.geo_data_loader import GeoDataLoader # Removido se não estiver em uso para evitar erro de importação
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
